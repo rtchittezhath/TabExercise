08163712317f7f373199e07153d79c9c05f6ebd6
Roshan <rtchittezhath@cegepgim.ca>
2020-04-18 23:20:44 -0400

    initial commit

---
+++ app/src/main/java/com/application/tabexercise/PagerAdapter.java
@@ -0,0 +1,32 @@
+package com.application.tabexercise;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(@NonNull FragmentManager fm, int numOfTabs) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        this.mNumOfTabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
---
+++ app/src/main/java/com/application/tabexercise/TabFragment2.java
@@ -0,0 +1,37 @@
+package com.application.tabexercise;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabFragment2 extends Fragment {
+
+
+    public TabFragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.tab_fragment2, container, false);
+
+
+        return view;
+
+    }
+
+    }
